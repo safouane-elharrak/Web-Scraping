18e86525158849ed0811c5b773cb798fd41649ca
unknown <safouane.elharrak1@gmail.com>
2022-03-25 16:55:52 +0100

    First commit

---
+++ ebay_scrap.py
@@ -0,0 +1,53 @@
+
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+URL = "https://www.ebay.com/sch/i.html?_nkw=mens+watches&_pgn="
+l = list()
+for page in  range(1,5) : 
+
+    html_text = requests.get(URL + str(page) + "#catalog-listing").content
+
+    soup = bs(html_text,'html.parser')
+
+    produits = soup.find_all('li', class_ ="s-item s-item__pl-on-bottom s-item--watch-at-corner")
+
+    for prod in produits :
+        info = prod.find("div",class_ ="s-item__info clearfix")  
+     
+        try :
+            title = info.find('a', class_='s-item__link').text
+        except:
+            title =''   
+        try :
+            price= info.find('span', class_="s-item__price").text 
+        except:
+            price =''
+        
+        detail= info.find('div', class_='s-item__details clearfix')
+
+        try :
+            sold = detail.find('span', class_='s-item__hotness s-item__itemHotness').text
+        except:
+            sold ='0 sold'
+        try :
+            delivry = detail.find('span', class_='s-item__shipping s-item__logisticsCost').text
+        except:
+            delivry =''
+        
+        try :
+            country = detail.find('span', class_='s-item__location s-item__itemLocation').text
+        except:
+            country =''
+
+        data = {"Title":title, "Country":country, "Price":price, "Sold":sold,"Delivry":delivry }
+                
+        l.append(data)
+      #  print(data)
+    
+df = pd.DataFrame(l)
+df.to_excel("/Users/Pro/Documents/Master MBD/MBD_S2/Web-Scraping/Ebay_Scrap/ebay.xlsx")
+
+print(df.tail())
